35a5f524d509b02aacec27a8b753d7e64e392b3d
qq_28991143 <qq_28991143@noreply.gitcode.com>
2025-04-20 23:30:50 +0800

    output to file

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-int main()
-{
-	freopen("source.lsl","r",stdin);
-	cin.read()
-}
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,22 @@
 use std::fs::File;
 
-use std::io::{BufReader,BufWriter,Write,Read,Lines,read_to_string};
+use std::io::{Write,read_to_string};
 use std::vec;
 
 /// 定义关键字表
-const reversed: &[&str] = &["auto","break","case",
+const REVERSED: &[&str] = &["auto","break","case",
 	"char","const","continue","default","do","double","else",
 	"enum","extern","float","for","goto","if","int","long","register","return",
 	"short","signed","sizeof","struct","switch","typedef","union","unsigned",
 	"void","volatile","while"];
 
 /// 定义运算符表
-const operator_fore: &[char] = &['+','-','*','/','=','<','>']; // 注意有 == 
+const OPERATOR_FORE: &[char] = &['+','-','*','/','=','<','>']; // 注意有 == 
 
 
 /// 定义分隔符表
 
-const splitter: &[char] = &['(',')','{','}','\n'];
+const SPLITTER: &[char] = &['(',')','{','}','\n'];
 
 fn main()
 {
@@ -24,6 +24,8 @@ fn main()
 	let mut identifier:Vec<String> = vec![];
 	// 定义常量表
 	let mut number:Vec<i32> = vec![];
+
+	let mut output = File::create("output").expect("err creating output file");
 
 	let source = File::open("source.lsl").expect("err reading file");
 	let text = read_to_string(source).expect("err read to string");
@@ -40,19 +42,22 @@ fn main()
 				c = iter.next().expect(&format!("标识符定义异常：{}",temp));
 			}
 			// 此时开始查表
-			if reversed.contains(&temp.as_str()) {
+			if REVERSED.contains(&temp.as_str()) {
 				// 生成保留字属性字
-				let attribute = reversed.iter().position(|&x| x==temp.as_str()).expect("不可能到达词法分析器的这里！");
-				println!("{}:<保留字,{}>",temp,attribute);
+				let attribute = REVERSED.iter().position(|&x| x==temp.as_str()).expect("不可能到达词法分析器的这里！");
+				// println!("{}:<保留字,{}>",temp,attribute);
+				output.write(format!("{}:<保留字,{}>\n",temp,attribute).as_bytes()).expect("write out err");
 			} else {
 				// 生成标识符属性字
 				if identifier.contains(&temp) {
 					let attribute = identifier.iter().position(|x|x==&temp).expect("???");
-					println!("{}:<标识符,{}>",temp,attribute);
+					// println!("{}:<标识符,{}>",temp,attribute);
+					output.write(format!("{}:<标识符,{}>\n",temp,attribute).as_bytes()).expect("write out err");
 				} else {
 					identifier.push(temp.clone());
 					let attribute = identifier.len() - 1;
-					println!("{}:<标识符,{}>",temp,attribute);
+					// println!("{}:<标识符,{}>",temp,attribute);
+					output.write(format!("{}:<标识符,{}>\n",temp,attribute).as_bytes()).expect("write out err");
 				}
 			}
 		} else if c.is_numeric() {
@@ -65,18 +70,21 @@ fn main()
 			// 生成常数属性字
 			if number.contains(&temp) {
 				let attribute = number.iter().position(|x|x==&temp).expect("???");
-				println!("{}:<常数,{}>",temp,attribute);
+				// println!("{}:<常数,{}>",temp,attribute);
+				output.write(format!("{}:<常数,{}>\n",temp,attribute).as_bytes()).expect("write out err");
 			} else {
 				number.push(temp.clone());
 				let attribute = number.len() - 1;
-				println!("{}:<常数,{}>",temp,attribute);
+				// println!("{}:<常数,{}>",temp,attribute);
+				output.write(format!("{}:<常数,{}>\n",temp,attribute).as_bytes()).expect("write out err");
 			}
-		} else if operator_fore.contains(&c) || splitter.contains(&c) {
+		} else if OPERATOR_FORE.contains(&c) || SPLITTER.contains(&c) {
 			if c=='=' && iter.clone().peekable().peek().unwrap()!=&' ' {
 				if let Some(x) = iter.next() {
 					if x == '=' {
 						// 生成属性字
-						println!("==:<运算符,0>");
+						// println!("==:<运算符,0>");
+						output.write(format!("==:<运算符,0>\n").as_bytes()).expect("write out err");
 					}else {
 						// error
 						panic!("={}:不是运算符",x);
@@ -84,17 +92,20 @@ fn main()
 				}
 			} else {
 				// 生成属性字
-				if operator_fore.contains(&c) {
+				if OPERATOR_FORE.contains(&c) {
 					// 运算符属性字
-					let mut attribute = operator_fore.iter().position(|x|x==&c).expect("???");
+					let mut attribute = OPERATOR_FORE.iter().position(|x|x==&c).expect("???");
 					attribute += 1; // 0 被 == 占用
-					println!("{}:<运算符,{}>",c,attribute);
+					// println!("{}:<运算符,{}>",c,attribute);
+					output.write(format!("{}:<运算符,{}>\n",c,attribute).as_bytes()).expect("write out err");
 				} else {
-					let attribute = splitter.iter().position(|x|x==&c).expect("???");
+					let attribute = SPLITTER.iter().position(|x|x==&c).expect("???");
 					if c == '\n' {
-						println!("\\n:<分隔符,{}>",attribute);
+						// println!("\\n:<分隔符,{}>",attribute);
+						output.write(format!("\\n:<分隔符,{}>\n",attribute).as_bytes()).expect("write out err");
 					} else {
-						println!("{}:<分隔符,{}>",c,attribute);
+						// println!("{}:<分隔符,{}>",c,attribute);
+						output.write(format!("{}:<分隔符,{}>\n",c,attribute).as_bytes()).expect("write out err");
 					}
 				}
 				c = iter.next().expect("无结束符号！");
@@ -108,21 +119,22 @@ fn main()
 	}
 
 
+	// 写文件
 	let mut buffer_reversed = File::create("table_reversed").expect("create reversed table err");
 	let mut buffer_operator = File::create("table_operator").expect("create operator table err");
 	let mut buffer_splitter = File::create("table_splitter").expect("create splitter table err");
 	let mut buffer_identifier = File::create("table_identifier").expect("create identifier table err");
 	let mut buffer_number = File::create("table_number").expect("create number table err");
 
-	let reversed_data = reversed.join(" ");
+	let reversed_data = REVERSED.join(" ");
 	buffer_reversed.write(reversed_data.as_bytes()).expect("write reveresed table err");
 
-	let operator_data = operator_fore.iter().map(|x|String::from(*x));
+	let operator_data = OPERATOR_FORE.iter().map(|x|String::from(*x));
 	let arr:Vec<String> = operator_data.collect();
-	let msg = arr.join(" ") + " ==";
+	let msg = "== ".to_owned() + arr.join(" ").as_str();
 	buffer_operator.write(msg.as_bytes()).expect("write operator table err");
 
-	let splitter_data = splitter.iter().map(|x|String::from(*x));
+	let splitter_data = SPLITTER.iter().map(|x|String::from(*x));
 	let arr:Vec<String> = splitter_data.collect();
 	let msg = arr.join(" ");
 	buffer_splitter.write(msg.as_bytes()).expect("write splitter table err");
